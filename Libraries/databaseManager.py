94c29c536033ce8a92e89e4dea1b4624e373aa06
Kibon <gabriel120026@hotmail.com>
2020-10-27 15:57:40 -0300

    Fix databaseManager.py

--- Libraries/databaseManager.py
+++ Libraries/databaseManager.py
@@ -74,7 +74,7 @@ class DatabaseManegementSQLite:
         user = cur.fetchall()
 
         if not user:
-            cur.execute("INSERT INTO Users(id, username) VALUES     (%s, %s)", (userID, username))
+            cur.execute("INSERT INTO Users(id, username) VALUES (?, ?)", (userID, username))
             print("Usuário novo adicionado: {}".format(username))
         else:
             print("Usuário encontrado: {}".format(username))
@@ -84,10 +84,13 @@ class DatabaseManegementSQLite:
     def setMbtiValue(self, mbtiValue, userId):
         conn = sqlite3.connect(self.DATABASE)
         cur = conn.cursor()
-        cur.execute("UPDATE Users SET mbti=(?) WHERE id=(?)", (mbtiValue, update.effective_user.id))
+        cur.execute("UPDATE Users SET mbti=(?) WHERE id=(?)", (mbtiValue, userId))
         conn.commit()
  
     def findMbtiCouples(self, response, username, userId):
+        conn = sqlite3.connect(self.DATABASE)
+        cur = conn.cursor()
+
         casais = {"ESTJ": "ISFP", "ISFP":"ESTJ",
                 "ISTJ": "ESFP", "ESFP":"ISTJ",
                 "INFP": "ENFJ", "ENFJ":"INFP",
@@ -97,8 +100,8 @@ class DatabaseManegementSQLite:
                 "ESFJ": "ISTP", "ISTP": "ESFJ",
                 "ENFP": "INTJ", "INTJ": "ENFP"}
 
-        self.cur.execute("SELECT mbti FROM Users WHERE id=(%s)", (userId,))
-        userMbtiTuple = self.cur.fetchall()
+        cur.execute("SELECT mbti FROM Users WHERE id=(?)", (userId,))
+        userMbtiTuple = cur.fetchall()
 
         companions = list()
         if not userMbtiTuple:
@@ -108,76 +111,13 @@ class DatabaseManegementSQLite:
 
         userMbti = list(userMbtiTuple[0])[0]
         
-        self.cur.execute("SELECT username FROM Users WHERE mbti=(%s)", (casais[userMbti],))
+        cur.execute("SELECT username FROM Users WHERE mbti=(?)", (casais[userMbti],))
 
-        matches = self.cur.fetchall()
+        matches = cur.fetchall()
 
         for user in matches:
             formatedCompanion = ''.join(map(str,user[0]))
             companions.append(formatedCompanion)
         
-        self.conn.commit()
+        conn.commit()
         return companions
-
-    def endConnection(self):
-        self.conn.commit()
-        self.conn.close()   
-
-
-"""
-
-def casalMBTI (update, context):
-    conn = sqlite3.connect(DATABASE)
-    cur = conn.cursor()
-
-    response = list()
-    casais = {"ESTJ": "ISFP", "ISFP":"ESTJ",
-            "ISTJ": "ESFP", "ESFP":"ISTJ",
-            "INFP": "ENFJ", "ENFJ":"INFP",
-            "INTP": "ENTJ", "ENTJ": "INTP",
-            "ESTP": "ISFJ", "ISFJ": "ESTP",
-            "ENTP": "INFJ", "INFJ": "ENTP",
-            "ESFJ": "ISTP", "ISTP": "ESFJ",
-            "ENFP": "INTJ", "INTJ": "ENFP"}
-
-    cur.execute("SELECT mbti FROM Users WHERE id=(%s)", (update.effective_user.id,))
-    userMbtiTuple = cur.fetchall()
-
-    companions = list()
-    if not userMbtiTuple:
-        print("Usuário @{} não cadastrado".format(update.effective_user.username,))
-        response.append("@{}, defina sua personalidade  MBTI antes com o comando mbti.".format(update.effective_user.id,))
-        return companions
-
-    userMbti = list(userMbtiTuple[0])[0]
-    cur.execute("SELECT username FROM Users WHERE mbti=(%s)", (casais[userMbti],))
-    matches = cur.fetchall()
-    for user in matches:
-        formatedCompanion = ''.join(map(str,user[0]))
-        companions.append(formatedCompanion)
-    conn.commit()
-
-    for text in response:
-        context.bot.send_message(chat_id=update.effective_chat.id,text=text)
-    return companions
-
-def casalpossivel (update, context):
-    companions = casalMBTI(update, context)
-    if companions:
-        companionList = "Lista de Companheiros:"
-        for companion in companions:
-            companionList += "{}".format(companion)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=companionList)
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Não há companheiros disponíveis para @{}.".format(update.effective_user.username))
-
-def parceiroMBTI (update, context):
-    companions = casalMBTI(update, context)
-    if companions:
-        companion = random.choice(companions)
-        context.bot.send_message(chat_id=update.effective_chat.id, text="O companheiro ideal do(a) @{} é: @{}.".format(update.effective_user.username, companion))
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Não há companheiros disponíveis para @{}.".format(update.effective_user.username))
-
-"""
-   
